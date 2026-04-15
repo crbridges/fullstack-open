281a128c5679f2cd4bb0571c46d6cf200bafbe95
cb <cbridges.coding@gmail.com>
2026-04-15 17:00:45 -0500

    complete 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -7,7 +13,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button text='Good' onClick={() => setGood(good + 1)} />
+      <Button text='Neutral' onClick={() => setNeutral(neutral + 1)} />
+      <Button text='Bad' onClick={() => setBad(bad + 1)} />
+      
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }

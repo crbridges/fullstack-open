b94d65473e0a8293dc5c7f0e7f2183edf284a3d2
cb <cbridges.coding@gmail.com>
2026-04-17 08:57:37 -0500

    complete 2.1

---
+++ part2/course_information2/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <ul>
+    {props.parts.map(p=> <Part key={p.id} part={p}/>)}
+  </ul>
+)
+
+const Part = (props) => (
+  <li>
+    {props.part.name} {props.part.exercises}
+  </li>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = ({course}) => {
+  return (
+    <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Pokemon training',
+        exercises: 121,
+        id: 4
+      },
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

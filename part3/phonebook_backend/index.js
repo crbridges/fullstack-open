4cd9bf517d9bb4fde1790b0f20168fbf8cb15136
cb <cbridges.coding@gmail.com>
2026-04-24 17:52:45 -0500

    added frontend to express static

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,10 +5,9 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
-
-
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :data'))
 app.use(cors())
+app.use(express.static("dist"))
 
 
 let phonebook = [
